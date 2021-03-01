dad8a3f01f8b3d5f64ba62329bad8337a8adb074
Sergio Ortiz <ortiz.sergio0303@gmail.com>
2021-02-28 20:45:29 -0500

    feat(functions): use faunadb npm package in the serverless functiontion to interact with faunadb database

--- functions/using-a-database.js
+++ functions/using-a-database.js
@@ -1,8 +1,17 @@
 const faunadb = require("faunadb");
+const { FAUNADB_SERVER_SECRET } = process.env;
+
+const client = new faunadb.Client({ secret: FAUNADB_SERVER_SECRET });
+const { Create, Collection } = faunadb.query;
 
 exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    body: "Hello World",
-  };
+  client
+    .query(
+      Create(Collection("myThoughts"), {
+        data: { thought: "I think therefore I am." },
+      })
+    )
+    .then((response) => {
+      return { statusCode: 200, body: response };
+    });
 };
